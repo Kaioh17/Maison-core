e03d2b061d97d4ae1bdbdd19cf5b79e35453e46b
mubaraq <mubskill@gmail.com>
2025-07-19 12:09:37 -0500

    update

--- backend/app/api/routers/drivers.py
+++ backend/app/api/routers/drivers.py
@@ -16,11 +16,7 @@ router = APIRouter(
     tags = ["Drivers"]
 )
 
-@router.post("/create", status_code=status.HTTP_201_CREATED,response_model= driver.DriverResponse)
-async def create_driver(payload: driver.DriverCreate, db: Session =  Depends(get_db)):
-    logger.info("Collecting info....")
-    driver = await driver_service.create_driver(payload, db)
-    return driver
+
 
 @router.patch("/register", status_code=status.HTTP_202_ACCEPTED,response_model= driver.DriverResponse)
 async def create_driver(payload: driver.DriverCreate, db: Session =  Depends(get_db)):
